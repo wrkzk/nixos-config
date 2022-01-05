437fd3a07e58726ea3c6756c9d53aad8870fe405
wrkzk <wrkzk@protonmail.com>
2022-01-04 22:48:41 -0600

    Add home-manager to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs }: {
-
+  outputs = { self, home-manager, nixpkgs }: {
     nixosConfigurations.sol = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
         [
           ./configuration.nix
           ./hardware-configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPackages = true;
+            home-manager.userUserPackages = true;
+            home-manager.users.warren = import ./home.nix;
+          }
         ];
     };
 
