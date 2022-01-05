6ad7af109a033d00ec3adf3666b4cb6adf6ece44
wrkzk <wrkzk@protonmail.com>
2022-01-04 22:14:25 -0600

    Modify flake.nix to remove container configuration

--- flake.nix
+++ flake.nix
@@ -1,28 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+  description = "A simple configuration for a NixOS system";
+  nixConfig.extra-experimental-features = "nix-command flakes";
+
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  }
 
   outputs = { self, nixpkgs }: {
 
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
-        [ ({ pkgs, ... }: {
-            boot.isContainer = true;
-
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "morty@example.org";
-            };
-          })
+        [
+          ./configuration.nix
+          ./hardware-configuration.nix
         ];
     };
 
