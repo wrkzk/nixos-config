9338cf1cd708560b876d13584f0e3e227e649afc
wrkzk <wrkzk@protonmail.com>
2022-01-04 23:07:20 -0600

    Add user password to system configuration

--- configuration.nix
+++ configuration.nix
@@ -49,9 +49,13 @@
   services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.warren = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  users = {
+    mutableUsers = false;
+    users.warren = {
+      isNormalUser = true;
+      hashedPassword = "$6$S5EukdL1T/iWf/.m$4qGh.MS5RqaSD3vWQepTkawdwRPxlVNl2ESeSUquLCp2Ym24UscLjHRZ.6DCov8AzNumDttojZCnpHIDeATYj/";
+      extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    };
   };
   
   nix.extraOptions = ''
