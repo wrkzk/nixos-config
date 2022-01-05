5a2b41aa1bbe3eaff25137cd532d2cd3235601b6
wrkzk <wrkzk@protonmail.com>
2022-01-04 22:14:54 -0600

    Add configuration files from non-flake system configuration

---
+++ configuration.nix
@@ -0,0 +1,134 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+	
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = [ "btrfs" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "sol";
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Awesome window manager.
+  services.xserver.displayManager.startx.enable = true;
+  services.xserver.windowManager.awesome.enable = true;
+  
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.warren = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+  
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    firefox
+    git
+    (pkgs.writeScriptBin "fsdiff" ''
+      #!${pkgs.stdenv.shell}
+      alias btrfs="${pkgs.btrfs-progs}/bin/btrfs"
+      alias sudo="${pkgs.sudo}/bin/sudo"
+      
+      set -euo pipefail
+      
+      OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
+      OLD_TRANSID=''${OLD_TRANSID#transid marker was }
+      
+      sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
+      sed '$d' |
+      cut -f17- -d' ' |
+      sort |
+      uniq |
+      while read path; do
+        path="/$path"
+        if [ -L "$path" ]; then
+          : # The path is a symbolic link, so is probably handled by NixOS already
+        elif [ -d "$path" ]; then
+          : # The path is a directory, ignore
+        else
+          echo "$path"
+        fi
+      done
+    '')
+  ];
+
+  environment.etc = {
+    nixos.source = "/persist/etc/nixos";
+    "NetworkManager/system-connections".source = "/persist/etc/NetworkManager/system-connections";
+    adjtime.source = "/persist/etc/adjtime";
+    NIXOS.source = "/persist/etc/NIXOS";
+    machine-id.source = "/persist/etc/machine-id";
+  };
+
+  systemd.tmpfiles.rules = [
+    "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
+    "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
+    "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
+  ];
+
+  security.sudo.extraConfig = ''
+    Defaults lecture = never
+  '';
+
+  boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
+    mkdir -p /mnt
+
+    mount -o subvol=/ /dev/mapper/enc /mnt
+
+    btrfs subvolume list -o /mnt/root |
+    cut -f9 -d' ' |
+    while read subvolume; do
+      echo "deleting /$subvolume subvolume..." &&
+      btrfs subvolume delete "/mnt/$subvolume"
+    done &&
+    echo "deleting /root subvolume..." &&
+    btrfs subvolume delete /mnt/root
+
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+    umount /mnt
+  ''; 
+
+  system.stateVersion = "21.11";
+
+}
+
