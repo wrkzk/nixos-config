97bdcbefb8737434bcb07f737f700862b1b39df5
wrkzk <wrkzk@protonmail.com>
2022-01-23 22:25:57 -0600

    Change bar active background color

--- modules/xorg/awesome/theme/theme.lua
+++ modules/xorg/awesome/theme/theme.lua
@@ -13,7 +13,7 @@ local theme = {}
 theme.font          = "sans 8"
 
 theme.bg_normal     = "#000000"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#15161e"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
