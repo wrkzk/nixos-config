cbd0aed180e7da42d4a3f1adecda8aed5a78deac
wrkzk <wrkzk@protonmail.com>
2022-01-24 14:04:35 -0600

    Install python packages

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -4,6 +4,9 @@ with pkgs;
 let
   python-packages = python-packages: with python-packages; [
     protobuf
+    requests
+    pyopenssl
+    certifi
   ];
   custom-python = python3.withPackages python-packages;
 in
