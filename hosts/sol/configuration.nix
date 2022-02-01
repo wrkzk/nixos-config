8e8f5e9517de376cfaa8af68aaeef0580ac2f237
wrkzk <wrkzk@protonmail.com>
2022-01-31 19:04:46 -0600

    Add monoid font

--- hosts/sol/configuration.nix
+++ hosts/sol/configuration.nix
@@ -96,7 +96,7 @@
   ];
 
   fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+    (nerdfonts.override { fonts = [ "Iosevka", "Monoid" ]; })
   ];
 
   environment.etc = {
