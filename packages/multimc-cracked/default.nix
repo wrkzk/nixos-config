6d9177fc37331727b06fc0e5f072da9ed02fdbd4
wrkzk <wrkzk@protonmail.com>
2022-03-01 10:48:16 -0600

    Update multimc package

--- packages/multimc-cracked/default.nix
+++ packages/multimc-cracked/default.nix
@@ -29,7 +29,7 @@ in stdenv.mkDerivation {
     chmod +x $out/bin/bin/UltimMC
 
     sed -i '36d' $out/bin/UltimMC
-    sed -i '16 a DATA_DIR="~/.local/share/multimc"' $out/bin/UltimMC
+    sed -i '16 a DATA_DIR=".local/share/multimc"' $out/bin/UltimMC
     sed -i '39 s/LAUNCHER_DIR}" "$@/DATA_DIR}" "$@/' $out/bin/UltimMC
   '';
 
