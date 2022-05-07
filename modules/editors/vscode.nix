879068b4623663540bd949cd36d7be946d04b3df
wrkzk <wrkzk@protonmail.com>
2022-05-07 14:01:06 -0500

    Switch vscode to vscode-fhs

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 
 let
-  extensions = with pkgs.vscode-extensions; [
-    ms-vscode.cpptools
-  ];
-  vscode-with-extensions = pkgs.vscode-with-extensions.override {
-    vscode = pkgs.vscode-fhs;
-    vscodeExtensions = extensions;
-  };
+  #extensions = with pkgs.vscode-extensions; [
+  #  ms-vscode.cpptools
+  #];
+  #vscode-with-extensions = pkgs.vscode-with-extensions.override {
+  #  vscode = pkgs.vscode-fhs;
+  #  vscodeExtensions = extensions;
+  #};
 in {
-  home.packages = [
-    vscode-with-extensions
+  home.packages = with pkgs; [
+    vscode-fhs
   ];
 }
