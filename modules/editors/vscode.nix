bff6eb0b2663779504225c3c21c2bfd1159bcb62
wrkzk <wrkzk@protonmail.com>
2022-05-07 13:41:31 -0500

    Install vscode cpptools extension

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 
 let
-  extensions = (with pkgs.vscode-extensions; []) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [];
+  extensions = (with pkgs.vscode-extensions; []) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    ms-vscode.cpptools
+  ];
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
     vscode = pkgs.vscode;
     vscodeExtensions = extensions;
