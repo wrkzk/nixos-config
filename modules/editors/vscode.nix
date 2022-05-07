aeb2b0915954335f5a9348736c0fbed2652abffe
wrkzk <wrkzk@protonmail.com>
2022-05-07 13:43:43 -0500

    Fix typo

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 let
-  extensions = (with pkgs.vscode-extensions; []) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+  extensions = with pkgs.vscode-extensions; [
     ms-vscode.cpptools
-  ];
+  ]
   vscode-with-extensions = pkgs.vscode-with-extensions.override {
     vscode = pkgs.vscode;
     vscodeExtensions = extensions;
