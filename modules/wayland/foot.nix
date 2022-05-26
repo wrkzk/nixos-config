c31e33ad66f48e4265341393e96c8b24371f3d4a
wrkzk <wrkzk@protonmail.com>
2022-05-25 23:23:36 -0500

    Change foot colorscheme

--- modules/wayland/foot.nix
+++ modules/wayland/foot.nix
@@ -5,8 +5,30 @@
     enable = true;
     server.enable = true;
     settings = {
-      font = "CaskaydiaCove Nerd Font:size=9.5";
-      dpi-aware = "no";
-    }
+      main = {
+        font = "CaskaydiaCove Nerd Font:size=9.3";
+        dpi-aware = "no";
+      };
+      colors = {
+        foreground = "c0caf5";
+        background = "1a1b26";
+        regular0 = "15161e";
+        regular1 = "f7768e";
+        regular2 = "9ece6a";
+        regular3 = "e0af68";
+        regular4 = "7aa2f7";
+        regular5 = "bb9af7";
+        regular6 = "7dcfff";
+        regular7 = "a9b1d6";
+        bright0 = "414868";
+        bright1 = "f7768e";
+        bright2 = "9ece6a";
+        bright3 = "e0af68";
+        bright4 = "7aa2f7";
+        bright5 = "bb9af7";
+        bright6 = "7dcfff";
+        bright7 = "c0caf5";
+      };
+    };
   };
 }
